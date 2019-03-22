678b365b38bd7da632da61f2362aae5348f629a2
Harsik <aznm12@gmail.com>
2019-03-22 17:40:18 +0900

    03.22

--- Alpha/src/main/java/com/example/Alpha/AlphaApplication.java
+++ Alpha/src/main/java/com/example/Alpha/AlphaApplication.java
@@ -2,10 +2,7 @@ package com.example.Alpha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; 
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class AlphaApplication {
 
