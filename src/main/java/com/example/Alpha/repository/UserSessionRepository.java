03e135ee870f6a18bfcde9376c07e092d6bfbf43
aznm12 <aznm12@archivsoft.com>
2019-04-26 09:15:11 +0900

    delete useless file

--- src/main/java/com/example/Alpha/repository/UserSessionRepository.java
+++
@@ -1,9 +0,0 @@
-// package com.example.Alpha.repository;
-
-// import com.example.Alpha.model.UserSession;
-
-// import org.springframework.data.repository.CrudRepository;
-
-// public interface UserSessionRepository extends CrudRepository<UserSession, Long> {
-//     UserSession findBySessionID(String sessionID);
-// }
